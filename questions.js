94571e4c67b20ee816093ea33fe15546f61c2536
iescuder4 <iescuder4@iespompeufabra.cat>
2025-12-19 09:02:32 +0100

    Update questions.js

--- questions.js
+++ questions.js
@@ -152,25 +152,25 @@ const questions = [
         correctAnswer: "d",
        steps = `
 \\[
-\\text{Capacitat nominal del tren} = 48 \, \text{passatgers}
+\\text{Capacitat nominal del tren} = 48 \\, \\text{passatgers}
 \\]
 \\[
-\\text{Interval entre sortides} = 15 \, \text{minuts}
+\\text{Interval entre sortides} = 15 \\, \\text{minuts}
 \\]
 \\[
-\\text{Temps disponible (1 hora)} = 60 \, \text{minuts}
+\\text{Temps disponible (1 hora)} = 60 \\, \\text{minuts}
 \\]
 \\[
-\\text{Nombre de sortides per hora} = \frac{60}{15} = 4
+\\text{Nombre de sortides per hora} = \\frac{60}{15} = 4
 \\]
 \\[
 \\text{Descomposició de la capacitat: } 48 = 40 + 8
 \\]
 \\[
-40 \times 4 = 160
+40 \\times 4 = 160
 \\]
 \\[
-8 \times 4 = 32
+8 \\times 4 = 32
 \\]
 \\[
 160 + 32 = 192
@@ -179,7 +179,7 @@ const questions = [
 \\text{Comprovació: el temps de trajecte (5 min) és menor que l'interval (15 min), per tant no limita la capacitat horària.}
 \\]
 \\[
-\\text{Nombre màxim de passatgers per hora} = 48 \times 4 = 192
+\\text{Nombre màxim de passatgers per hora} = 48 \\times 4 = 192
 \\]
  <br><br>
             <img src="ruta/a/imatgeResposta.jpg" alt="Imatge relacionada amb la resposta">`,  // Imatge opcional al final de la resposta
